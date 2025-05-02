56d63e736151239b8a792a5cf8c1daf5b9493a42
Ali Shah <109840507+alishah1998@users.noreply.github.com>
2025-05-02 03:12:50 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_fshCoc8X0', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4t3d70h6bm648a1pdr4peu9b9e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_wSYrKl0zh', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '20gkso65on9g89i4tfh8cs45t6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
